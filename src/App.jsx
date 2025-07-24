0bba9244ca907eebc91fabc93b0e6c1b73a49e0b
yuanhangshe <yuanhangshe@tencent.com>
2025-07-24 22:23:41 +0800

    feat: perf

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Experience } from "./components/Experience";
 import { UI } from "./components/UI";
-
+import { Perf } from "r3f-perf";
 function App() {
   return (
     <>
       <UI />
-      <Loader />
-      <Canvas shadows camera={{
+      {/* <Loader /> */}
+
+      <Canvas
+        shadows
+        camera={{
           position: [-0.5, 1, window.innerWidth > 800 ? 4 : 9],
           fov: 45,
-        }}>
+        }}
+      >
+        <Perf position="top-left" matrixUpdate deepAnalyze overClock />
         <group position-y={0}>
           <Suspense fallback={null}>
             <Experience />
